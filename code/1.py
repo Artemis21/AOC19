5e04634e4bab00ce17b6f84acf59460844d0f4e7
Artemis <hollyshort21@gmail.com>
2019-12-03 19:32:12 +0000

    Added 1

---
+++ code/1.py
@@ -0,0 +1,31 @@
+import math
+
+
+def fuel(mass):
+    return math.floor(mass / 3) - 2
+
+def complete_fuel(mass):
+    ret = 0
+    while True:
+        mass = math.floor(mass / 3) - 2
+        if mass < 0:
+            break
+        ret += mass
+    return ret
+
+
+def get_inp():
+    with open('1.txt') as f:
+        return tuple(map(int, f))
+
+
+def part_a():
+    return sum(map(fuel, get_inp()))
+
+def part_b():
+    return sum(map(complete_fuel, get_inp()))
+
+
+if __name__ == '__main__':
+    print('1A:', part_a())
+    print('1B:', part_b())
